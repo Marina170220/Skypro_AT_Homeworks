ed9170fb245230414c25ce1a6b568f0f289eac7e
Marina Petruk <petrukmn@yandex.ru>
2024-07-21 20:57:09 +0300

    Add lesson 5

---
+++ Lesson5/task3.6.py
@@ -0,0 +1,31 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome = webdriver.Chrome()
+firefox = webdriver.Firefox()
+
+try:
+    chrome.get("http://the-internet.herokuapp.com/login")
+    firefox.get("http://the-internet.herokuapp.com/login")
+    
+    input_name = chrome.find_element(By.ID, "username").send_keys("tomsmith")
+    sleep(1)
+    input_name = firefox.find_element(By.ID, "username").send_keys("tomsmith")
+    sleep(1)
+
+    input_pass = chrome.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    sleep(1)
+    input_pass = firefox.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    sleep(1)
+
+    button = chrome.find_element(By.TAG_NAME, "button").click()
+    sleep(1)
+    button = firefox.find_element(By.TAG_NAME, "button").click()
+    sleep(1)
+
+except Exception as ex:
+    print(ex)
+finally:
+    chrome.quit()
+    firefox.quit()
